cfb015f3669d2a801bc4409786576ecb6d55aeb6
Kamil Wójcik <wojcikakamil@gmail.com>
2022-08-15 13:31:45 +0200

    Added right side panel, added editor background

--- EDWebApp/src/module/editor/components/EditorSidePanel.svelte
+++ EDWebApp/src/module/editor/components/EditorSidePanel.svelte
@@ -1,32 +1,58 @@
 <script lang="ts">
-    import {beforeUpdate, onMount} from "svelte";
+    import {preferencesStore} from "../../../shared/stores/PreferencesStore";
 
+    const max = 340;
+    const min = 100;
+    export let position: 'l' | 'r' = 'l';
     let width = 160;
+    let hidden = false;
 
     const resizeStart = (ev: MouseEvent) => {
         let startWidth = width;
         let startPosition = ev.x;
         const resize = (ev: MouseEvent) => {
-            width = Math.min(Math.max(startWidth + ev.x- startPosition, 20), 280);
+            if (position === 'l' && ev.x < window.innerWidth * 0.05 || position === 'r' && ev.x > window.innerWidth * 0.95) {
+                hidden = true;
+                return;
+            } else {
+                hidden = false;
+            }
+
+            let diff: number = position == "l"?
+                ev.x - startPosition :
+                startPosition - ev.x;
+
+            width = Math.min(Math.max(startWidth + diff, min), max);
         };
         window.addEventListener('pointermove', resize);
         window.addEventListener('mouseup', () => window.removeEventListener('pointermove', resize));
     }
 
+    const getTopOffset = () => {
+        return position === 'r' && $preferencesStore.minimalNavbar? '10px': '60px';
+    }
+
 </script>
 
-<div class="side-panel bg-primary-400" style={`width: ${width}px`}>
+{#if !hidden}
+<div class={`side-panel fixed top-0 bg-primary-400 ${position === 'r'? 'right-0': ''}`}  style={`width: ${width}px; padding-top: ${getTopOffset()}`}>
     <slot></slot>
     <div on:mousedown={resizeStart}
-        class="h-full w-4 bg-color-l2 absolute right-0 top-0 hover:cursor-w-resize center">
+        class={`h-full w-4 bg-color-l2 absolute top-0 hover:cursor-w-resize center ${position === 'r'? 'left-0': 'right-0'}`}>
         <p class="select-none text-sm text-black dark:text-white font-extrabold">&GT;</p>
     </div>
 </div>
+{:else}
+    <div on:mousedown={resizeStart}
+         class={`h-full w-4 bg-color-l2 absolute top-0 hover:cursor-w-resize center ${position === 'r'? 'right-0': 'left-0'}`}>
+        <p class="select-none text-sm text-black dark:text-white font-extrabold">&GT;</p>
+    </div>
+{/if}
 
 <style>
 
     div {
-        height: calc(100vh - 56px);
+        height: 100vh;
     }
 
 </style>
--- EDWebApp/src/routes/editor/index.svelte
+++ EDWebApp/src/routes/editor/index.svelte
@@ -3,15 +3,38 @@
     import Editor from "../../module/editor/components/Editor.svelte";
     import {DiagramElement} from "../../module/editor/logic/DiagramElement";
     import {UmlClass, UmlClassState} from "../../module/editor/components/elements/UmlClass";
-    import {ElementType} from "../../module/editor/logic/ElementType";
 
     let elements: DiagramElement<any>[] = [
         new UmlClass(new UmlClassState('Class 2', 0, 0, 100, 80), s => {})
     ]
 </script>
 
+<div class="background-color bg-gray-200 dark:bg-gray-800"></div>
+<div class="background"></div>
+
 <div class="z-10">
-    <EditorSidePanel/>
+    <EditorSidePanel>
+        <div class="text-2xl">Hello world</div>
+    </EditorSidePanel>
 </div>
 
 <Editor editorId="0" elements={elements}></Editor>
+
+<div class="z-10">
+    <EditorSidePanel position="r">
+        <div class="text-2xl">Hello world</div>
+    </EditorSidePanel>
+</div>
+
+<style>
+    .background {
+        @apply fixed w-[100vw] h-[100vh] top-0 left-0;
+        background-image: url('/Circuit Board.svg');
+        background-size: 140%;
+        opacity: 0.5;
+    }
+
+    .background-color {
+        @apply fixed w-[100vw] h-[100vh] top-0 left-0;
+    }
+</style>
